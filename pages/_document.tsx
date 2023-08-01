72e9ec75f6844edc2614ba794288741bf3c8acac
davidquintr <davidquintr97@gmail.com>
2023-08-01 13:33:42 -0600

    og graph added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,22 @@ import Link from 'next/link'
 import { FaGithub } from 'react-icons/fa'
 
 export default function Document() {
+  let deployUrl = "https://whatsapp-reader-next.vercel.app/"
   return (
     <Html lang="en">
-      <Head />
-      <body className='bg-darkbg min-h-screen relative flex flex-col'>
+      <Head>
+      <meta property="og:title" content="WhatsApp Reader Next"></meta>
+        <meta property="og:description" content="When you export your WA chat, turns chaotic read the conversations WA Reader gives the format to the conversations and shows like the original chat."></meta>
+        <link rel="shortcut icon" href="./iconreader.png"></link>
+        <meta property="og:type" content="interpreter"></meta>
+        <meta property="og:locale" content="en_US"></meta>
+        <title>WhatsApp Reader Next</title>
+        <meta property="twitter:card" content="summary"></meta>
+        <meta property="twitter:title" content="WhatsApp Reader"></meta>
+        <meta property="og:image" content={`${deployUrl}thumbnail.png`}></meta>
+        <meta property="og:url" content={`${deployUrl}`}></meta>
+      </Head>
+        <body className='bg-darkbg min-h-screen relative flex flex-col'>
         <Main />
         <footer className='flex flex-col justify-center items-center text-brightgreen bg-darkgreen p-[10px] text-center gap-[8px] relative mt-auto'>
           <p>©2023 Designed and Programmed by David Quintanilla</p>
