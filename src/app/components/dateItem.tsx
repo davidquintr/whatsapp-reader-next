14340adbbe0ed2ca0f026d53bcdb091615f6538c
davidquintr <davidquintr97@gmail.com>
2024-06-14 10:37:00 -0600

    trying to fix hydratation error

--- src/app/components/dateItem.tsx
+++ src/app/components/dateItem.tsx
@@ -8,7 +8,7 @@ const DateItem = ({ date }: DateItemProps) => {
     <>
       <li className="flex text-center items-center justify-center">
         <p className="bg-light-gray-200 shadow-md text-black/90 dark:bg-dark-gray-50 dark:text-white/90 text-sm rounded-md px-5">
-          {date?.toLocaleString("default", {
+          {date?.toLocaleString("en-US", {
             month: "long",
             day: "2-digit",
             year: "numeric",
